139a7e7e8eeccd84b28ff640febcc182bb757838
Youngrae Cho <ip323@naver.com>
2023-12-31 15:22:38 +0900

    HTTP 요청 응답 기록

--- Actuator/src/main/java/hello/ActuatorApplication.java
+++ Actuator/src/main/java/hello/ActuatorApplication.java
@@ -1,7 +1,9 @@
 package hello;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ActuatorApplication {
@@ -10,4 +12,9 @@ public class ActuatorApplication {
         SpringApplication.run(ActuatorApplication.class, args);
     }
 
+    //단순하고 기능에 제한이 많아서 개발 단계에서만 주로 사용한다.
+    @Bean
+    public InMemoryHttpExchangeRepository httpExchangeRepository() {
+        return new InMemoryHttpExchangeRepository();
+    }
 }
